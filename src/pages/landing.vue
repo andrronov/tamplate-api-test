a9b6781db24c5a082da101945dda349859e6bc5f
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-22 11:25:33 +0300

    фиксы после деплоя

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -8,11 +8,10 @@
          <img src="/img/neon.jpeg" class="w-full min-h-screen object-cover" alt="">
        </div>
  
-       <!-- 5 REASONS -->
          <pageElement>
             <h2 class="text-dev-100 text-3xl font-medium text-center my-10">4 причины попробовать это API</h2>
             <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-4 place-items-center gap-4">
-               <reason v-for="(reason, index) in reasons" :key="index" :title="reason.title" :description="reason.description" />
+               <reasonComponent v-for="(reason, index) in reasons" :key="index" :title="reason.title" :description="reason.description" />
             </div>
          </pageElement>
  
@@ -44,7 +43,7 @@
 <script setup>
 import pageWrapper from '../components/UI/pageWrapper.vue';
 import pageElement from '../components/UI/pageElement.vue'
-import reason from '../components/reasonComponent.vue'
+import reasonComponent from '../components/reasonComponent.vue'
 import pageButton from '../components/UI/pageButton.vue'
 import { ref } from 'vue';
 
@@ -66,6 +65,7 @@ const reasons = [
         description: "Мы предлагаем активную поддержку и обширное сообщество разработчиков, готовых помочь вам в решении любых вопросов. Вы можете рассчитывать на нашу команду, которая всегда готова ответить на ваши запросы и предоставить необходимую помощь."
     }
 ]
+
 const exampleButtons = [
 {
     title: 'Все юзеры',
