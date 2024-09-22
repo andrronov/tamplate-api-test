179f1bbbaace6d4a26949739539b4d93c01081ef
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-22 11:58:35 +0300

    фиксы после деплоя

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/template-api-test'
+  base: '/tamplate-api-test'
 })
