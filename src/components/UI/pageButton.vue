ec9d952b875312bca9677e76d0d68a11eb10022a
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-22 11:59:55 +0300

    adding dist subtree

--- src/components/UI/pageButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-   <button @click="$router.push(`${link}`)" class="px-3 cursor-pointer py-2 text-center rounded-xl border border-white hover:bg-dev-500 hover:text-white transition-all duration-300 ease-in-out">
-      <slot></slot>
-   </button>
-</template>
-<script setup>
-const props = defineProps({link: {type: String}})
-</script>
--- src/components/UI/pageElement.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-dev-500 w-full py-12 px-2 border-y border-dev-100">
-    <div class="mx-auto max-w-7xl text-dev-100 flex flex-col items-center">
-      <slot />
-    </div>
-  </div>
-</template>
-
-<script setup></script>
